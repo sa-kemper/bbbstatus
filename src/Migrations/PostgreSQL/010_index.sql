ed6cfe20a18fadb80106d5c0a79bf1a50a6bdfb2
sa-kemper <196162023+sa-kemper@users.noreply.github.com>
2025-02-09 11:04:24 +0100

    Add index migrations, remove InitialUp.sql (is deprecated).
    Signed-off-by: sa-kemper <196162023+sa-kemper@users.noreply.github.com>

--- src/Migrations/PostgreSQL/010_index.sql
+++ src/Migrations/PostgreSQL/010_index.sql
@@ -1,3 +1,4 @@
+-- +goose Up
 CREATE INDEX idx_poll_responses_poll_id ON poll_responses (poll_id);
 CREATE INDEX idx_user_events_internal_meeting_id ON user_events (internal_meeting_id);
 CREATE INDEX idx_chat_messages_internal_meeting_id ON chat_messages (internal_meeting_id);
@@ -5,4 +6,12 @@ CREATE INDEX idx_polls_internal_meeting_id ON polls (internal_meeting_id);
 CREATE INDEX idx_meetings_external_id ON meetings (external_meeting_id);
 CREATE INDEX idx_chat_messages_meeting_time ON chat_messages (internal_meeting_id);
 CREATE INDEX idx_polls_meeting ON polls (internal_meeting_id);
-#
+-- +goose Down
+DROP INDEX idx_poll_responses_poll_id;
+DROP INDEX idx_user_events_internal_meeting_id;
+DROP INDEX idx_chat_messages_internal_meeting_id;
+DROP INDEX idx_polls_internal_meeting_id;
+DROP INDEX idx_meetings_external_id;
+DROP INDEX idx_chat_messages_meeting_time;
+DROP INDEX idx_polls_meeting;
+

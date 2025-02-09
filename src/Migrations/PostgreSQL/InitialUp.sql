8d6d4a55a484cda4495d29d25c0250f7d1e9b7cf
sa-kemper <196162023+sa-kemper@users.noreply.github.com>
2025-02-09 10:49:56 +0100

    Add meetingEvents migrations
    Signed-off-by: sa-kemper <196162023+sa-kemper@users.noreply.github.com>

--- src/Migrations/PostgreSQL/InitialUp.sql
+++ src/Migrations/PostgreSQL/InitialUp.sql
@@ -1,11 +1,3 @@
-CREATE TABLE meeting_events
-(
-    event_id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    internal_meeting_id VARCHAR(255) NOT NULL,
-    event_type          VARCHAR(50)  NOT NULL,
-    event_timestamp     TIMESTAMP    NOT NULL,
-    FOREIGN KEY (internal_meeting_id) REFERENCES meetings (internal_meeting_id)
-);
 
 CREATE TABLE user_events
 (

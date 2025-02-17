a3c1ce616b8f8f78873c6639066e512c7c92d05f
sa-kemper <196162023+sa-kemper@users.noreply.github.com>
2025-02-17 14:53:10 +0100

    Implement the get recordings methode.
    Signed-off-by: sa-kemper <196162023+sa-kemper@users.noreply.github.com>

---
+++ src/internal/BBBAPI/GetRecordings.go
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025 Samuel Kemper
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package BBBAPI
+
+import (
+	"context"
+	"encoding/xml"
+	"net/http"
+)
+
+func (a *API) GetRecordings(ctx context.Context, MeetingID string) (result Recordings, err error) {
+	var client = a.getHTTPClient()
+	request, err := http.NewRequestWithContext(ctx, "GET", generateURL(URLConfig{Hostname: a.Hostname, Methode: "getRecordings", Parameters: map[string]string{}, SharedSecret: a.SharedSecret}), nil)
+	response, err := client.Do(request)
+	if err != nil {
+		return Recordings{}, err
+	}
+
+	err = xml.NewDecoder(response.Body).Decode(&result)
+	if err != nil {
+		return Recordings{}, err
+	}
+
+	return result, nil
+}

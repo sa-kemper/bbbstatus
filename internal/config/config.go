0b08938a14c2dc185c9e3cc44661bf3692e4d89c
sa-kemper <196162023+sa-kemper@users.noreply.github.com>
2026-04-03 10:14:29 +0200

    Add configuration struct with helper functions
    Signed-off-by: sa-kemper <196162023+sa-kemper@users.noreply.github.com>

---
+++ internal/config/config.go
@@ -0,0 +1,117 @@
+package config
+
+import (
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+type ConfigurationStruct struct {
+	BaseConfig       BaseConfig
+	ReportConfig     ReportConfig
+	DatabaseConfig   DbConfig
+	BBBServers       []BbbServer
+	ScaleLiteServers []ScaleliteServer
+}
+
+func (c *ConfigurationStruct) GetBBBServer(hostname string) *BbbServer {
+	for i, bbbServer := range c.BBBServers {
+		if bbbServer.Hostname == hostname {
+			return &c.BBBServers[i]
+		}
+	}
+	return nil
+}
+
+func (c *ConfigurationStruct) FindBBBServers(name string) (matches []BbbServer) {
+	matches = make([]BbbServer, 0)
+	for _, bbbServer := range c.BBBServers {
+		if strings.Contains(name, bbbServer.Hostname) {
+			matches = append(matches, bbbServer)
+		}
+	}
+	return matches
+}
+
+func (c *ConfigurationStruct) FindScaleliteServers(name string) (matches []ScaleliteServer) {
+	matches = make([]ScaleliteServer, 0)
+	for _, scaleLite := range c.ScaleLiteServers {
+		if strings.Contains(name, scaleLite.Hostname) {
+			matches = append(matches, scaleLite)
+		}
+	}
+	return matches
+}
+
+func (c *ConfigurationStruct) GetScaleliteServer(hostname string) *ScaleliteServer {
+	for i, scaleLite := range c.ScaleLiteServers {
+		if scaleLite.Hostname == hostname {
+			return &c.ScaleLiteServers[i]
+		}
+	}
+	return nil
+}
+
+type BaseConfig struct {
+	Host               string `toml:"HOST" env:"HOST" env-default:"0.0.0.0" env-description:"Host to bind bbbstatus to"`
+	Port               string `toml:"PORT" env:"PORT" env-default:"8080" env-description:"Port to bind bbbstatus to"`
+	ServeStaticContent bool   `toml:"SERVE_STATIC_CONTENT" env:"SERVE_STATIC_CONTENT" env-default:"true" env-description:"Serve static content using the bbbstatus binary"`
+	TrustedProxies     string `toml:"TRUSTED_PROXIES" env:"TRUSTED_PROXIES" env-default:"" env-description:"Trusted proxies if behind a reverse proxy (recommended) bbbstatus to"`
+	ServerLang         string `toml:"SERVER_LANG" env:"SERVER_LANG" env-default:"en" env-description:"The Server language is a fallback language"`
+	ClearQueue         bool   `toml:"CLEAR_QUEUE" env:"CLEAR_QUEUE" env-default:"false" env-description:"Clears the queue of bbb-webhook events, all of them will be dropped"`
+}
+
+type ReportConfig struct {
+	CsvStructure string `toml:"CSV_STRUCTURE" env:"CSV_STRUCTURE" env-default:"time,user,action,text representation" env-description:"CSV order of elements"`
+}
+
+type DbConfig struct {
+	DatabaseConnectionString string `toml:"DB_CONNECTION_STRING" env:"DB_CONNECTION_STRING" env-description:"Database (PostgreSQL) connection string"`
+}
+
+type BbbServer struct {
+	Hostname     string `toml:"HOSTNAME" env-required:"true"`      // required
+	ApiPort      string `toml:"API_PORT"`                          // required only if not 443
+	SharedSecret string `toml:"SHARED_SECRET" env-required:"true"` // required only if API usage is needed
+	APITimeout   int    `toml:"API_TIMEOUT"`                       // defaults to 2 (seconds)
+	FriendlyName string `toml:"FRIENDLY_NAME"`                     // not required
+}
+
+type ScaleliteServer struct {
+	Hostname     string `toml:"HOSTNAME"`
+	ApiPort      string `toml:"API_PORT"`
+	SharedSecret string `toml:"SHARED_SECRET"`
+	APITimeout   int    `toml:"API_TIMEOUT"`
+	FriendlyName string `toml:"FRIENDLY_NAME"`
+}
+
+func (s BbbServer) isEqual(other BbbServer) bool {
+	return s.Hostname == other.Hostname &&
+		s.ApiPort == other.ApiPort &&
+		s.SharedSecret == other.SharedSecret &&
+		s.FriendlyName == other.FriendlyName
+}
+
+var defaultConfiguration = ConfigurationStruct{
+	BaseConfig:     BaseConfig{Host: "0.0.0.0", Port: "8080", ServeStaticContent: true},
+	ReportConfig:   ReportConfig{CsvStructure: "time,user,action,text representation"},
+	DatabaseConfig: DbConfig{DatabaseConnectionString: "postgres://bbbstatus:bbbstatus@localhost/bbbstatus"},
+	BBBServers: []BbbServer{
+		{Hostname: "localhost", ApiPort: "443", SharedSecret: "aDefaultSharedSecret", APITimeout: 5},
+		{Hostname: "other-host", ApiPort: "443", SharedSecret: "aDefaultSharedSecret", APITimeout: 2},
+	},
+}
+
+type CustomContext struct {
+	echo.Context
+	Config *ConfigurationStruct
+}
+
+func ConfigMiddleware(cfg *ConfigurationStruct) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := &CustomContext{c, cfg}
+			return next(cc)
+		}
+	}
+}
